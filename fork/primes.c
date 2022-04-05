90d91022c2d56a39128df3f2fb3b4c2dc732788d
facundoluzzi <facundoluzz@gmail.com>
2022-04-05 20:45:32 -0300

    Resuelvo primes

--- fork/primes.c
+++ fork/primes.c
@@ -1,7 +1,74 @@
-int
-main(int argc, char *argv[])
-{
-	// Your code here
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-	return 0;
+void recursividad(int file_descriptor, int number){
+    int recv;
+    int first_message;
+    if (read(file_descriptor, &first_message, sizeof(first_message)) < 0) {
+        perror("[P] Error en Read");
+        _exit(-1);
+    }
+    if (first_message < number && first_message > 0){
+        printf("primo %d\n", first_message);
+        int fds_on_recursivity[2];
+        int v = pipe(fds_on_recursivity);
+        if (v < 0){
+            _exit(-1);
+        }
+        int i = fork();
+
+        if (i == 0){
+            close(file_descriptor);
+            close(fds_on_recursivity[1]);
+            recursividad(fds_on_recursivity[0], number);
+            close(fds_on_recursivity[0]);
+        } else{
+            close(fds_on_recursivity[0]);
+            while(read(file_descriptor, &recv, sizeof(recv))) {
+                if (recv % first_message != 0){
+                    if (write(fds_on_recursivity[1], &recv, sizeof(recv)) < 0) {
+                        perror("[P] Error en Write\n");
+                        _exit(-1);
+                    }
+                }
+            }
+            close(fds_on_recursivity[1]);
+            close(file_descriptor);
+            wait(NULL);
+        }
+    }
+}
+
+
+int
+main(int argc, char** argv)
+{	
+    int number = atoi(argv[1]);
+    int file1[2];
+    int v = pipe(file1);
+    if (v < 0){
+        _exit(-1);
+    }
+    int fork1 = fork();
+
+    if (fork1 == 0){
+        close(file1[1]);
+        recursividad(file1[0], number);
+        close(file1[0]);
+    }
+    if (fork1 > 0) {
+        close(file1[0]);
+        int n = 2;
+        while (n <= number) {
+            write(file1[1], &n, sizeof(n));
+            n++;
+        }
+        close(file1[1]);
+        wait(NULL);
+    }
+    
+    return 0;
 }
